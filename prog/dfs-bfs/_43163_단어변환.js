d093216b6932bf34346952ff329632334ca9b39e
정진아 <human00024@naver.com>
2022-02-23 14:08:00 +0900

    [review] _43163.단어변환

--- prog/dfs-bfs/_43163_단어변환.js
+++ prog/dfs-bfs/_43163_단어변환.js
@@ -9,45 +9,58 @@
  * @param {*} begin
  * @param {*} target
  * @param {*} words
- * @returns
+ * @returns :최소 몇 단계의 과정을 거쳤는지 반환, 변환할 수 없는 경우 0 반환
+ *
+ *
+ * ### 리뷰
+ * - 풀이 과정은 다음과 같다.
+ *   현재 단어에서 변환 가능한 단어를 대기열에 추가한다.
+ *   대기열에 추가한 단어를 순차적으로 꺼내서 변환 준비를 한다.
+ *   target 단어와 동일할 때까지 위의 과정을 반복한다.
+ *
+ * - visited 배열에 객체 형태 {"단어":false}로 넣는 것 보다는 index로 접근하는게 더 빠를 것이라 생각했다.
+ *   이를 위해, 기존 words 배열에 begin을 추가한 새로운 배열을 생성하였다.
+ *
+ * - 다른 사람과의 풀이와 유사하다!
  */
 
-function solution(begin, target, words) {
-    return bfs(begin, target, words);
-}
-
-function bfs(begin, target, originWords) {
+function solution(begin, target, originWords) {
     const words = [begin, ...originWords];
+    const visited = Array(words.length).fill(false);
 
-    const N = words.length;
-
-    const visited = Array(N).fill(false);
     const qu = [0];
     visited[0] = true;
 
     let changeCount = 0;
-    while (qu.length > 0) {
-        const SIZE = qu.length;
-        for (let s = 0; s < SIZE; s++) {
-            const word = words[qu.shift()];
-            if (word === target) {
-                return changeCount;
-            }
-            for (let i = 0; i < N; i++) {
-                if (visited[i] || !isDiffSingeAlpabet(word, words[i])) continue;
-                qu.push(i);
-                visited[i] = true;
-            }
+    while (qu.length) {
+        for (let s = 0, SIZE = qu.length; s < SIZE; s++) {
+            const currentIndex = qu.shift();
+            const currentWord = words[currentIndex];
+            visited[currentIndex] = true;
+
+            if (currentWord === target) return changeCount;
+            addNextWordsInQueue(words, currentWord, qu, visited);
         }
         changeCount++;
     }
     return 0;
 }
 
-function isDiffSingeAlpabet(begin, target) {
+function addNextWordsInQueue(words, currentWord, queue, visited) {
+    words.forEach((nextWord, nextIndex) => {
+        if (!isChangeWord(visited[nextIndex], currentWord, nextWord)) return;
+        queue.push(nextIndex);
+    });
+}
+
+function isChangeWord(isVisited, origin, target) {
+    return !isVisited && isDiffSingeAlpabet(origin, target);
+}
+
+function isDiffSingeAlpabet(origin, target) {
     let diffCount = 0;
-    for (let i = 0, N = begin.length; i < N; i++) {
-        if (begin[i] !== target[i]) diffCount++;
+    for (let i = 0, N = origin.length; i < N; i++) {
+        if (origin[i] !== target[i]) diffCount++;
     }
     return diffCount <= 1;
 }
