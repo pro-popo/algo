b4a63a2fea727698ab92f17e6efcb26fd8fff1ac
정진아 <human00024@naver.com>
2022-02-23 12:27:41 +0900

    [review] _43165.타겟넘버

--- prog/dfs-bfs/_43165_타겟넘버.js
+++ prog/dfs-bfs/_43165_타겟넘버.js
@@ -7,6 +7,16 @@
  * @param {*} numbers :사용할 수 있는 숫자들의 배열
  * @param {*} target  :타겟 넘버
  * @returns  :타겟 넘버를 만드는 방법의 수
+ *
+ *
+ * ### 리뷰
+ * - 다른 사람 코드와 거의 유사하다!
+ * - 전역변수(targetCount, N)을 어디에 둘지에 대해 고민을 했다.
+ *   main함수에 두고 매개변수로 전달하면, 전달 인자가 너무 많아져 코드 복잡도가 높아졌다.
+ *   전역변수로 분리하고자 외부에 배치했다. 대신, solution 함수에서 초기화 하는 추가 작업이 필요하다.
+ *   또 다른 방법으로는, dfs함수를 inner function로 작성하는 방법이 있을 것 같다!
+ *   (많은 풀이 방식에서 이 방식을 선택했으나,
+ *    내부 함수로 작성하면 오히려 함수를 분리하지 않는 것과 동일한 복잡도를 가진다는 생각이 들었다.)
  */
 
 let N = 0;
@@ -16,7 +26,7 @@ function solution(numbers, target) {
     N = numbers.length;
     targetCount = 0;
 
-    dfs(numbers, 0, 0, target, targetCount);
+    dfs(numbers, 0, 0, target);
 
     return targetCount;
 }
