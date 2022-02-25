b5e925cc8fe2aa29fef28017a944d1d880656a98
정진아 <human00024@naver.com>
2022-02-25 12:55:20 +0900

    [review] _42839.소수찾기

--- prog/bruteforce/_42839_소수찾기.js
+++ prog/bruteforce/_42839_소수찾기.js
@@ -1,19 +1,30 @@
 /**
  * 각 종이 조각에 적힌 숫자로 만들 수 있는 소수가 몇 개인가?
  *
- * @param {*} numbers :숫자 배열 (1~7의 자리)
+ * @param {*} numbers :숫자 배열 (길이 1~7)
  * @returns : 몇 개의 소수가 만들어지는지
+ *
+ * ### 리뷰
+ * - 풀이 방식은 다음과 같다.
+ *   먼저, 각 숫자로 만들 수 있는 조합을 구한다. (중복 제거)
+ *   구한 조합을 가지고 소수인지 판단한다.
+ *
+ * - 소수인지 판단하기 위해서,
+ *   해당 숫자의 √N 까지 확인하면, 시간 복잡도는 O(√N)이 된다.
+ *   √N는 해당 숫자의 약수의 중간값을 구하는 방법이다.
+ *
+ * - 다른 사람 풀이와 유사!
  */
 
 function solution(numbers) {
     const result = new Set();
     combination([...numbers], '', (usedPapers = 0), result);
 
-    return [...result].filter(isPrime).length;
+    return countingPrime(result);
 }
 
 function combination(numbers, currentNumber, usedPapers, result) {
-    if (currentNumber.length === numbers.length) return;
+    if (isMaxNumberLength(currentNumber, numbers.length)) return;
 
     numbers.forEach((number, index) => {
         if (isUsedPaper(usedPapers, index + 1)) return;
@@ -29,8 +40,16 @@ function combination(numbers, currentNumber, usedPapers, result) {
     });
 }
 
+function isMaxNumberLength(number, maxLength) {
+    return number.length === maxLength;
+}
+
 function isUsedPaper(usedPapers, paperIndex) {
     return usedPapers & (1 << paperIndex);
+}
+
+function countingPrime(numbers) {
+    return [...numbers].filter(isPrime).length;
 }
 
 function isPrime(number) {
