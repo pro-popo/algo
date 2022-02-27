d8532c354a1e019bc0a1f943584ecb08a2d3a865
정진아 <human00024@naver.com>
2022-02-27 19:16:58 +0900

    [review] _42862.체육복

--- prog/greedy/_42862_체육복.js
+++ prog/greedy/_42862_체육복.js
@@ -7,15 +7,32 @@
  * @param {*} lost 도난당한 학생들의 번호 배열
  * @param {*} reserve 여벌의 체육복을 가진 학생들의 번호 배열
  * @returns 체육수업을 들을 수 있는 학생의 최댓값
+ *
+ *
+ * ### 리뷰
+ * - 풀이 방식은 다음과 같다.
+ *   먼저 여벌의 체육복을 잃어버린 학생들을 제외한다. (lost와 reserve 둘 다 존재하는 학생)
+ *   앞, 뒤 순서로 여벌의 체육복이 존재하는지 검사한 후, 체육복을 빌린다.
+ *   마지막까지 체육복을 빌리지 못한 학생들만 뽑아낸다.
+ *
+ * - 문제를 풀면서 두 가지 포인트를 놓쳤다.
+ *   첫 번째는, lost가 기본적으로 오름차순으로 입력이 들어오지 않는다는 점.
+ *   두 번째는, 여벌의 체육복을 잃어버린 학생이 존재할 수 있다는 점이다.
+ *
+ * - 다른 사람 문제 중,
+ *   reserve.find(r => Math.abs(r-a) <= 1)와 같이 Math.abs로 앞/뒤 번호를 찾는 방법도 있었다.
+ *
+ * - reserve를 boolean 배열로 바꿀 필요없이 그대로 사용할 수 있다.
+ *   그러면, 더 짧은 코드로 작성할 수 있으나, 사용한 여벌에 대해 기존 배열에서 제거하는 과정이 필요하다.
  */
 
 function solution(n, lost, reserve) {
     const isReserve = changeToBooleanArray(reserve, n);
 
     lost.sort(ASC);
-    lost = removePersonLostReserve(lost, isReserve);
+    lost = removeStudentsLostReserve(lost, isReserve);
 
-    return n - countPersonWithoutQymClothes(lost, isReserve);
+    return n - countStudentWithoutQymClothes(lost, isReserve);
 }
 
 function changeToBooleanArray(numbers, n) {
@@ -27,7 +44,7 @@ function changeToBooleanArray(numbers, n) {
 
 const ASC = (a, b) => a - b;
 
-function removePersonLostReserve(lost, isReserve) {
+function removeStudentsLostReserve(lost, isReserve) {
     return lost.filter((student) => {
         if (isReserve[student]) {
             isReserve[student] = false;
@@ -37,7 +54,7 @@ function removePersonLostReserve(lost, isReserve) {
     });
 }
 
-function countPersonWithoutQymClothes(lost, isReserve) {
+function countStudentWithoutQymClothes(lost, isReserve) {
     return lost.filter((studentNumber) => {
         const [front, back] = [studentNumber - 1, studentNumber + 1];
         if (isReserve[front]) {
