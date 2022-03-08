7919020b5ab4c1449c35aa0c4f237493fbe6ca2a
정진아 <human00024@naver.com>
2022-03-08 14:55:53 +0900

    [refactor] _92341.주차 요금 계산 - 변수명 수정, 함수 및 변수 추출

--- prog/2022-kakao-blind/_92341_주차요금계산.js
+++ prog/2022-kakao-blind/_92341_주차요금계산.js
@@ -2,48 +2,62 @@
  * 차량별로 주차 요금을 계산하자.
  * - 입차된 후, 출차된 내역이 없으면 23:59에 출차
  * - 기본 시간을 초과할 경우, 추가 요금 발생
- *   초과 시간 % 단위 시간 != 0dls ruddn, dhffla
+ *   초과 시간 % 단위 시간 !== 0 인 경우, 올림
+ *
  * @param {*} fees 주차 요금 [기본시간(분), 기본 요금, 단위 시간(분), 단위 요금]
  * @param {*} records 자동차의 입/출차 내역 [[시:분, 차량번호, (입차 | 출차)]]
  * @returns 차량 번호가 작은 자동차부터 청구할 주차 요금 배열 반환
  */
 
 function solution(fees, records) {
-    const [defaultTime, defaultPrice, unitTime, unitPrice] = fees;
     const parkingLot = new Map();
     const totalUsedTime = {};
 
     records.forEach((record) => {
         const [time, car, type] = record.split(' ');
-        const [hour, minute] = time.split(':').map(Number);
-        const timeInMinute = hour * 60 + minute;
+        const minute = convertToMinute(time);
 
         switch (type) {
             case 'IN':
-                parkingLot.set(car, timeInMinute);
+                parkingLot.set(car, minute);
                 break;
             case 'OUT':
-                const usedTime = timeInMinute - parkingLot.get(car);
+                const usedTime = minute - parkingLot.get(car);
                 totalUsedTime[car] = (totalUsedTime[car] || 0) + usedTime;
                 parkingLot.delete(car);
         }
     });
 
+    clearCarInParkingLot(parkingLot, totalUsedTime);
+
+    return Object.entries(totalUsedTime)
+        .sort(ASC_CAR)
+        .map(([_, usedTime]) => calculateFee(fees, usedTime));
+}
+
+function convertToMinute(time) {
+    const [hour, minute] = time.split(':').map(Number);
+    return hour * 60 + minute;
+}
+
+function clearCarInParkingLot(parkingLot, totalUsedTime) {
     const LAST_TIME = 23 * 60 + 59;
     for (const [car, enteredTime] of parkingLot) {
         usedTime = LAST_TIME - enteredTime;
         totalUsedTime[car] = (totalUsedTime[car] || 0) + usedTime;
     }
+}
 
-    return Object.entries(totalUsedTime)
-        .sort(([carA], [carB]) => +carA - +carB)
-        .map(([_, usedTime]) => {
-            if (usedTime <= defaultTime) return defaultPrice;
-            return (
-                defaultPrice +
-                Math.ceil((usedTime - defaultTime) / unitTime) * unitPrice
-            );
-        });
+const ASC_CAR = ([carA], [carB]) => +carA - +carB;
+
+function calculateFee(fees, usedTime) {
+    const [defaultTime, defaultPrice, unitTime, unitPrice] = fees;
+
+    return (
+        defaultPrice +
+        (usedTime > defaultTime &&
+            Math.ceil((usedTime - defaultTime) / unitTime) * unitPrice)
+    );
 }
 
 console.log(
