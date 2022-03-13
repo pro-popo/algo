1ea79a037c81c6d29cb83d006db99e2fdcdc9388
정진아 <human00024@naver.com>
2022-03-13 15:03:25 +0900

    [refactor] _11660.구간 합 구하기 5 - 함수 추출, 매개변수 줄이기

--- boj/prefix-sum/_11660_구간합구하기5.js
+++ boj/prefix-sum/_11660_구간합구하기5.js
@@ -3,11 +3,13 @@
  * (x1, y1)부터 (x2, y2)까지 합을 구하는 프로그램을 작성하자.
  *
  * @param {*} ([numbers, ranges]) [수들의 2차원 배열, 구간(x, y)]
+ *            (1 ≤ N ≤ 1024, 1 ≤ M ≤ 100,000)
  * @returns x번째 수부터 y번째 수까지의 합
  */
 
 function solution([map, ranges]) {
     const cumulativeSum = calculateCumulativeSum(map);
+
     return ranges
         .map(
             ([x1, y1, x2, y2]) =>
@@ -21,20 +23,30 @@ function solution([map, ranges]) {
 
 function calculateCumulativeSum(map) {
     const N = map.length;
-    const cumulativeSum = Array.from(Array(N + 1), () => Array(N + 1).fill(0));
+    const cumulativeSum = Array.from(Array(N + 1), (_, index) =>
+        index === 0 ? Array(N + 1).fill(0) : [0, ...map[index - 1]],
+    );
 
-    for (let i = 1; i <= N; i++) {
-        for (let j = 1; j <= N; j++) {
-            cumulativeSum[i][j] = cumulativeSum[i][j - 1] + map[i - 1][j - 1];
-        }
-    }
+    sumLeftToRight(cumulativeSum);
+    sumTopToBottom(cumulativeSum);
 
-    for (let j = 1; j <= N; j++) {
-        for (let i = 1; i <= N; i++) {
-            cumulativeSum[i][j] += cumulativeSum[i - 1][j];
-        }
-    }
     return cumulativeSum;
+
+    function sumLeftToRight(cumulativeSum) {
+        for (let i = 1; i <= N; i++) {
+            for (let j = 1; j <= N; j++) {
+                cumulativeSum[i][j] += cumulativeSum[i][j - 1];
+            }
+        }
+    }
+
+    function sumTopToBottom(cumulativeSum) {
+        for (let j = 1; j <= N; j++) {
+            for (let i = 1; i <= N; i++) {
+                cumulativeSum[i][j] += cumulativeSum[i - 1][j];
+            }
+        }
+    }
 }
 
 function input(test) {
