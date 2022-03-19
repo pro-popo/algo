18b73bea18909d96c65a4042ff448b5c363956a9
정진아 <human00024@naver.com>
2022-03-19 20:50:25 +0900

    [upload] _82612.부족한 금액 계산하기

---
+++ prog/challenges/_82612_부족한금액계산하기.js
@@ -0,0 +1,22 @@
+/**
+ * 놀이기구의 원래 이용료는 price원이지만,
+ * N번째 이용한다면, 원래 이용료의 N배를 받고자 한다.
+ * [EX] 100원 => 2번, 200원 => 3번, 300원
+ *
+ * 놀이기구를 count번 타게 될 때, 가지고 있는 금액에서 얼마나 모자라는지 구하자.
+ * 만약, 금액이 부족하지 않다면 0을 반환한다.
+ *
+ * @param {*} price 놀이기구 이용료 (1~2_500)
+ * @param {*} money 가지고 있는 금액 (1~1_000_000_000)
+ * @param {*} count 놀이기구의 이용 횟수 (1~2_500)
+ * @returns 부족한 금액 반환, 부족하지 않다면 0 반환
+ */
+
+function solution(price, money, count) {
+    do {
+        money -= price * count;
+    } while (count-- > 0);
+    return money >= 0 ? money : -money;
+}
+
+console.log(solution(3, 1_000_000_000, 4));
