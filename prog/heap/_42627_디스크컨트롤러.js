a447fe9bdcc360e2afbc1c396627c75c63aaec4b
정진아 <human00024@naver.com>
2022-03-28 15:38:19 +0900

    [refactor] _42627.디스크 컨트롤러 - 변수명 및 함수명 수정, 함수 추출, 로직 분리, 조건문 분리

--- prog/heap/_42627_디스크컨트롤러.js
+++ prog/heap/_42627_디스크컨트롤러.js
@@ -6,41 +6,79 @@
  * @param {*} jobs [작업이 요청되는 시점, 작업의 소요시간]
  * @returns 작업의 요청부터 종료까지 걸린 시간의 평균을 가장 줄이는 방법으로 처리할 때의 평균
  */
+
 function solution(jobs) {
-    const JOB_LENGTH = jobs.length;
-    jobs.sort(([requestTime], [other]) => requestTime - other);
+    return averageProcessTime(sumTime, jobs.length);
+}
+
+function averageProcessTime(sumTime, numberOfJob) {
+    const sumTime = sumProcessTimeOfAllJob(jobs);
+    return Math.floor(sumTime / numberOfJob);
+}
+
+function sumProcessTimeOfAllJob(jobs) {
+    let remainJobs = [...jobs].sort(ASC_REQUEST_TIME);
 
     const queue = [];
     let [time, sumTime] = [0, 0];
 
     do {
-        let index = jobs.findIndex(([requestTime]) => requestTime > time);
-        if (!queue.length && index === 0 && ++time) continue;
-        if (index === -1) index = jobs.length;
+        const [requestedJobs, notRequestedJobs] =
+            divideByRequestJobs(remainJobs);
+        if (isNotRequestedAllJob(requestedJobs) && ++time) continue;
 
-        queue.push(...jobs.slice(0, index));
-        jobs = jobs.slice(index);
+        remainJobs = notRequestedJobs;
+        queue.push(...requestedJobs);
+        queue.sort(ASC_REQUIRED_TIME);
 
-        queue.sort(
-            ([, requiredTime], [, otherRequired]) =>
-                requiredTime - otherRequired,
-        );
+        [time, sumTime] = processJob(queue.shift());
+    } while (remainJobs.length || queue.length);
 
-        const [requestTime, requiredTime] = queue.shift();
-        time += requiredTime;
-        sumTime += time - requestTime;
-    } while (jobs.length || queue.length);
+    return sumTime;
 
-    return Math.floor(sumTime / JOB_LENGTH);
+    function divideByRequestJobs(jobs) {
+        let index = findNotRequestJob();
+        return [jobs.slice(0, index), jobs.slice(index)];
+    }
+
+    function findNotRequestJob() {
+        let index = remainJobs.findIndex(isNotRequestJob);
+        if (isRequestedAllJob()) index = remainJobs.length;
+        return index;
+
+        function isNotRequestJob([requestTime]) {
+            return requestTime > time;
+        }
+        function isRequestedAllJob() {
+            return index === -1;
+        }
+    }
+
+    function isNotRequestedAllJob(requestedJobs) {
+        return !queue.length && !requestedJobs.length;
+    }
+
+    function processJob(job) {
+        const [requestTime, requiredTime] = job;
+        return [
+            time + requiredTime,
+            sumTime + time + requiredTime - requestTime,
+        ];
+    }
 }
 
-// console.log(
-//     solution([
-//         [0, 3],
-//         [1, 9],
-//         [2, 6],
-//     ]),
-// );
+const ASC_REQUEST_TIME = ([requestTime], [other]) => requestTime - other;
+const ASC_REQUIRED_TIME = ([, requiredTime], [, other]) => requiredTime - other;
+
+/****** TEST CASE *******/
+
+console.log(
+    solution([
+        [0, 3],
+        [1, 9],
+        [2, 6],
+    ]),
+);
 
 console.log(
     solution([
