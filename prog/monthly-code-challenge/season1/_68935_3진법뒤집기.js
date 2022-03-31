c633373ac744be4c80a9fe83d1dfd44d6ac90952
정진아 <human00024@naver.com>
2022-03-31 23:58:46 +0900

    [review] _68935_3진법 뒤집기

--- prog/monthly-code-challenge/season1/_68935_3진법뒤집기.js
+++ prog/monthly-code-challenge/season1/_68935_3진법뒤집기.js
@@ -4,6 +4,12 @@
  *
  * @param {*} n 자연수
  * @returns
+ *
+ * ### 리뷰
+ * - 풀이 방식은 다음과 같다.
+ *   Number의 toString과 parseInt 함수를 사용하여 원하는 진법으로 변환한다.
+ *
+ * - JavsScript로 진법 변환하는 방식을 알면 쉽게 풀 수 있는 문제다!
  */
 
 function solution(n) {
