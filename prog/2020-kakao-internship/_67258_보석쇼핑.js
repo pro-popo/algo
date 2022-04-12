009680ee891e32271887f004718bd6353d3d743d
정진아 <human00024@naver.com>
2022-04-12 18:30:07 +0900

    [review] _67258.보석 쇼핑

--- prog/2020-kakao-internship/_67258_보석쇼핑.js
+++ prog/2020-kakao-internship/_67258_보석쇼핑.js
@@ -7,6 +7,41 @@
  * @param {*} gems 진열대 번호 순서대로 보석들의 이름이 저장된 배열 (1~100_000)
  * @returns 가장 짧은 구간의 [시작 진열대 번호, 끝 진열대 번호]
  *
+ * ### 리뷰
+ * - 풀이 방식은 다음과 같다.
+ *   이 문제는 투 포인터 알고리즘으로 해결할 수 있다.
+ *
+ *   먼저, Gem의 종류의 개수를 구한다. (Set 객체 활용)
+ *   그 다음 선택한 gem을 저장할 Map객체를 생성하고, (보석번호: 개수)
+ *   start 포인터와 end 포인터를 [0, -1]에 둔다.
+ *
+ *   그리고 end가 gems 범위를 넘어갈 때까지 아래 과정을 반복한다.
+ *
+ *   start부터 end까지의 gem을 선택했을 때,
+ *   모든 gem의 종류를 전부 선택한 상태인지 검사한다.
+ *
+ *   만약 모든 gem의 종류를 선택하지 않은 상태라면,
+ *   end를 증가시켜 선택 범위를 넓힌다.
+ *
+ *   만약 모든 gem의 종류를 선택한 상태라면,
+ *   answer에 더 짧은 구간을 저장한 다음,
+ *   start를 증가시켜 선택 범위를 줄인다.
+ *
+ *   위 과정을 거치면 가장 짧은 구간을 구할 수 있다.
+ *
+ * - gems의 범위가 최대 100_000이기 때문에,
+ *   모든 경우를 탐색할 수 없다. (100_000 * 100_000)
+ *
+ * - 다른 풀이 방식으로는,
+ *   투 포인터를 사용하지 않고, Map 객체만 사용해서 구할 수 있다.
+ *   먼저, 선택한 gem을 저장하는 Map 객체를 생성한다. (보석이름: index)
+ *
+ *   그리고 gems를 순회하여,
+ *   해당 gem을 Map 객체에 추가한다.
+ *   만약 Map 객체에 저장된 gem이 있다면, 기존 값은 제거한 뒤에 추가한다.
+ *   이때, Map 객체의 size가 Gem의 종류의 개수와 동일한 경우,
+ *   길이가 작은 경우로 answer를 갱신한다.
+ *   answer = min([map.value()[0], index], answer)
  */
 
 function solution(gems) {
@@ -71,7 +106,7 @@ console.log(
         'DIA',
     ]),
 );
-console.log(solution(['AA', 'AB', 'AC', 'AA', 'AC']));
-console.log(solution(['XYZ', 'XYZ', 'XYZ']));
-console.log(solution(['ZZZ', 'YYY', 'NNNN', 'YYY', 'BBB']));
-console.log(solution(['A', 'B', 'A', 'B']));
+// console.log(solution(['AA', 'AB', 'AC', 'AA', 'AC']));
+// console.log(solution(['XYZ', 'XYZ', 'XYZ']));
+// console.log(solution(['ZZZ', 'YYY', 'NNNN', 'YYY', 'BBB']));
+// console.log(solution(['A', 'B', 'A', 'B']));
