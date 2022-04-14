a1b825ce1f599666200a8841a08f3de3dae85a4b
정진아 <human00024@naver.com>
2022-04-15 00:53:58 +0900

    [refactor] _67260.동굴 탐험 - 함수 추출

--- prog/2020-kakao-internship/_67260_동굴탐험.js
+++ prog/2020-kakao-internship/_67260_동굴탐험.js
@@ -31,12 +31,7 @@
  */
 
 function solution(n, path, order) {
-    const graph = Array.from(Array(n), () => []);
-    path.forEach(([node, other]) => {
-        graph[node].push(other);
-        graph[other].push(node);
-    });
-
+    const graph = createGraph(n, path);
     const highPrioirty = Array(n).fill(null);
     order.forEach(
         ([prioirtyNode, node]) => (highPrioirty[node] = prioirtyNode),
@@ -45,6 +40,7 @@ function solution(n, path, order) {
     const queue = [0];
     const visited = new Set();
     const lowPriority = Array(n).fill(null);
+
     while (queue.length) {
         const node = queue.shift();
 
@@ -61,7 +57,20 @@ function solution(n, path, order) {
 
         visited.add(node);
     }
-    return visited.size === n;
+    return isAllVisited();
+
+    function isAllVisited() {
+        return visited.size === n;
+    }
+}
+
+function createGraph(n, edges) {
+    const graph = Array.from(Array(n), () => []);
+    edges.forEach(([node, other]) => {
+        graph[node].push(other);
+        graph[other].push(node);
+    });
+    return graph;
 }
 
 /****** TEST CASE *******/
@@ -87,43 +96,43 @@ console.log(
     ),
 );
 
-// console.log(
-//     solution(
-//         9,
-//         [
-//             [8, 1],
-//             [0, 1],
-//             [1, 2],
-//             [0, 7],
-//             [4, 7],
-//             [0, 3],
-//             [7, 5],
-//             [3, 6],
-//         ],
-//         [
-//             [4, 1],
-//             [5, 2],
-//         ],
-//     ),
-// );
+console.log(
+    solution(
+        9,
+        [
+            [8, 1],
+            [0, 1],
+            [1, 2],
+            [0, 7],
+            [4, 7],
+            [0, 3],
+            [7, 5],
+            [3, 6],
+        ],
+        [
+            [4, 1],
+            [5, 2],
+        ],
+    ),
+);
 
-// console.log(
-//     solution(
-//         9,
-//         [
-//             [0, 1],
-//             [0, 3],
-//             [0, 7],
-//             [8, 1],
-//             [3, 6],
-//             [1, 2],
-//             [4, 7],
-//             [7, 5],
-//         ],
-//         [
-//             [4, 1],
-//             [8, 7],
-//             [6, 5],
-//         ],
-//     ),
-// );
+console.log(
+    solution(
+        9,
+        [
+            [0, 1],
+            [0, 3],
+            [0, 7],
+            [8, 1],
+            [3, 6],
+            [1, 2],
+            [4, 7],
+            [7, 5],
+        ],
+        [
+            [4, 1],
+            [8, 7],
+            [6, 5],
+        ],
+    ),
+);
