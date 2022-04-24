98e6ac5372eb87025e25e0422937cda6fdbaf0fa
정진아 <human00024@naver.com>
2022-04-25 00:05:47 +0900

    [review] _76501.음양 더하기

--- prog/monthly-code-challenge/season2/_76501_음양더하기.js
+++ prog/monthly-code-challenge/season2/_76501_음양더하기.js
@@ -3,6 +3,12 @@
  * @param {*} absolutes 정수들의 절댓값들
  * @param {*} signs 정수들의 부호들 (true: 양수, false: 음수)
  * @returns 실제 정수들의 합
+ *
+ * ### 리뷰
+ * - 풀이 방식은 다음과 같다.
+ *   정수들의 절댓값을 순회하여,
+ *   해당 정수가 양수인 경우는 +1, 음수인 경우는 -1을 곱한다.
+ *   그 다음, 모든 정수들의 합을 구하면 된다.
  */
 
 function solution(absolutes, signs) {
