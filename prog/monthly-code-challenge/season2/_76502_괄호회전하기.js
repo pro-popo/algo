a9939cf18a89dd7fdd64b8f3f408b498312303be
정진아 <human00024@naver.com>
2022-04-25 23:33:05 +0900

    [upload] _76502.괄호 회전하기

---
+++ prog/monthly-code-challenge/season2/_76502_괄호회전하기.js
@@ -0,0 +1,46 @@
+/**
+ * 올바른 괄호 문자열은 다음 규칙을 지킨다.
+ * - (), [], {}는 모두 올바른 괄호 문자열이다.
+ * - A가 올바른 괄호 문자열이라면 (A)도 올바른 괄호 문자열이다.
+ * - A와 B가 올바른 괄호 문자열이라면, AB도 올바른 문자열이다.
+ *
+ * 올바른 괄호 문자열이 아니라면,
+ * 왼쪽으로 x칸만큼 회전시켜 올바른 괄호 문자열로 만들어야 한다.
+ *
+ * @param {*} s 괄호로 이루어진 문자열 (1~1_000)
+ * @returns s를 회전시켰을 때 나타나는 올바른 괄호 문자열의 개수
+ */
+function solution(s) {
+    let answer = 0;
+    let numberOfRorate = s.length;
+    do {
+        const rotated = s.slice(numberOfRorate) + s.slice(0, numberOfRorate);
+        if (isCorrectBracket(rotated)) answer++;
+    } while (--numberOfRorate);
+
+    return answer;
+}
+
+function isCorrectBracket(s) {
+    const bracket = new Map([
+        ['(', ')'],
+        ['[', ']'],
+        ['{', '}'],
+    ]);
+
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if ([...bracket.keys()].includes(s[i])) {
+            stack.push(s[i]);
+            continue;
+        }
+        if (bracket.get(stack.pop()) === s[i]) continue;
+
+        return false;
+    }
+    return stack.length ? false : true;
+}
+
+console.log(solution('[](){}'));
+console.log(solution('}]()[{'));
+console.log(solution('('));
