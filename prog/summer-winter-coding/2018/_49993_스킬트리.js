4ab258de68f681a6b97db4dea8ff5a864eb2855a
정진아 <human00024@naver.com>
2022-05-14 21:32:58 +0900

    [upload] _49993.스킬트리

---
+++ prog/summer-winter-coding/2018/_49993_스킬트리.js
@@ -0,0 +1,27 @@
+/**
+ * 선행 스킬이란, 스킬을 배우기 전 먼저 배워야 하는 스킬을 의미한다.
+ * [EX] 스파크 → 라이트닝 볼트 → 썬더
+ * 이때, 순서에 없는 다른 스킬은 순서에 상관없이 배울 수 있다.
+ *
+ * @param {*} skill 선행 스킬 순서 (1~20)
+ * @param {*} skill_trees 유저들이 만든 스킬트리 (1,20)
+ * @returns 가능한 스킬트리 개수
+ */
+
+function solution(priorSkill, skillTrees) {
+    return skillTrees.filter(isPossibleSkill).length;
+
+    function isPossibleSkill(skillTree) {
+        const skills = skillTree.replace(
+            new RegExp(`[^${priorSkill}]`, 'g'),
+            '',
+        );
+
+        let pointer = 0;
+        return [...skills].every(skill => skill === priorSkill[pointer++]);
+    }
+}
+
+/****** TEST CASE *******/
+
+console.log(solution('CBD', ['BACDE', 'CBADF', 'AECB', 'BDA']));
