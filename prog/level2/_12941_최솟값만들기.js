deb1fef72c4c79ac8aaeb43ba29eec917d7dcf13
정진아 <human00024@naver.com>
2022-05-28 03:09:55 +0900

    [upload] _12941.최솟값 만들기

---
+++ prog/level2/_12941_최솟값만들기.js
@@ -0,0 +1,23 @@
+/**
+ * 길이가 같은 자연수 배열 A, B가 있다.
+ * 두 배열에서 각각 한 개의 숫자를 뽑아 두 수를 곱한다.
+ * 이 과정을 배열의 길이만큼 반복하며, 두 수를 곱한 값을 누적하여 더한다.
+ * 이때 최종적으로 누적된 값이 최소가 되도록 만들고자 한다.
+ * 단, 각 배열에서 K번째 숫자를 뽑았다면, 해당 숫자는 다시 뽑을 수 없다.
+ *
+ * @param {number[]} A
+ * @param {number[]} B
+ * @returns 최종적으로 누적된 최솟값
+ */
+
+function solution(A, B) {
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+
+    return A.reduce((sum, _, i) => sum + A[i] * B[i], 0);
+}
+
+/****** TEST CASE *******/
+
+console.log(solution([1, 4, 2], [5, 4, 4]));
+console.log(solution([1, 2], [3, 4]));
