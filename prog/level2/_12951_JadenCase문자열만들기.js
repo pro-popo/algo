dd7929d34f2c3ea0ff591a13d1bcbae46de27db5
정진아 <human00024@naver.com>
2022-05-31 02:05:41 +0900

    [upload] _12951.JadenCase 문자열 만들기

---
+++ prog/level2/_12951_JadenCase문자열만들기.js
@@ -0,0 +1,25 @@
+/**
+ * JadenCase란 모든 단어의 첫 문자가 대문자이고,
+ * 그 이외의 알파벳은 소문자인 문자열이다.
+ * 단, 첫 문자가 알파벳이 아닐 경우 그대로 해당 문자를 출력한다.
+ *
+ * @param {string} s - 문자열 (1~200)
+ * @returns  {string} - JadenCase로 바꾼 문자열
+ */
+
+function solution(s) {
+    return s
+        .split(' ')
+        .map(word => {
+            if (!word) return word;
+            if (word.length === 1) return word[0].toUpperCase();
+            return word[0].toUpperCase() + word.slice(1).toLowerCase();
+        })
+        .join(' ');
+}
+
+/****** TEST CASE *******/
+
+console.log(solution('3people unFollowed me'));
+console.log(solution('for the last week'));
+console.log(solution('for   the last week'));
