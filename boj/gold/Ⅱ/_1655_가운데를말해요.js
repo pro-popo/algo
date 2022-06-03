d7d056c57b6f75dce6a16786114cff0cd8256f53
정진아 <human00024@naver.com>
2022-06-03 17:54:03 +0900

    [upload] _1655.가운데를 말해요 - 성공 코드

--- boj/gold/Ⅱ/_1655_가운데를말해요.js
+++ boj/gold/Ⅱ/_1655_가운데를말해요.js
@@ -4,32 +4,130 @@
  * - 동생은 백준이가 지금까지 말한 수 중에서 중간값을 말한다.
  *   만약 외친 수의 개수가 짝수개라면 중간에 있는 두 수 중에서 작은 수를 말한다.
  *
- * @param {} input - N: 1~100,000
+ * @param {[number, number[]]} input - 백준이가 말한 수들, N: 1~100,000
  */
 
 function solution(input) {
     const [, numbers] = [input.shift(), input.map(Number)];
 
-    const maxHeap = [];
-    const minHeap = [];
+    const maxHeap = new PriorityQueue(new Comparator((a, b) => b - a));
+    const minHeap = new PriorityQueue(new Comparator((a, b) => a - b));
+
     let answer = '';
     numbers.forEach(number => {
         if (maxHeap.length === minHeap.length) {
-            maxHeap.push(number);
-            maxHeap.sort((a, b) => a - b);
+            maxHeap.enqueue(number);
         } else {
-            minHeap.push(number);
-            minHeap.sort((a, b) => a - b);
+            minHeap.enqueue(number);
         }
 
-        if (maxHeap[0] > minHeap[0]) {
-            [maxHeap[0], minHeap[0]] = [minHeap[0], maxHeap[0]];
+        if (maxHeap.top > minHeap.top) {
+            const [max, min] = [maxHeap.dequeue(), minHeap.dequeue()];
+            maxHeap.enqueue(min);
+            minHeap.enqueue(max);
         }
 
-        answer += maxHeap[maxHeap.length - 1] + '\n';
+        answer += maxHeap.top + '\n';
     });
 
     return answer;
+}
+
+class PriorityQueue {
+    values = [];
+    constructor(comparator = new Comparator()) {
+        this.comparator = comparator;
+    }
+
+    get top() {
+        return this.values[0];
+    }
+
+    get length() {
+        return this.values.length;
+    }
+
+    enqueue(value) {
+        this.values.push(value);
+        this.bubbleUp();
+    }
+
+    dequeue() {
+        const top = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return top;
+    }
+
+    bubbleUp() {
+        let childIndex = this.length - 1;
+        const child = this.values[childIndex];
+        while (childIndex > 0) {
+            const parentIndex = Math.floor((childIndex - 1) / 2);
+            const parent = this.values[parentIndex];
+
+            if (this.comparator.compare(parent, child) < 0) break;
+            this.values[parentIndex] = child;
+            this.values[childIndex] = parent;
+            childIndex = parentIndex;
+        }
+    }
+
+    sinkDown() {
+        const parent = this.values[0];
+        let parentIndex = 0;
+
+        while (true) {
+            let swapIndex = null;
+
+            let [leftIndex, rightIndex] = [
+                2 * parentIndex + 1,
+                2 * parentIndex + 2,
+            ];
+
+            if (
+                leftIndex < this.length &&
+                this.comparator.compare(parent, this.values[leftIndex]) > 0
+            ) {
+                swapIndex = leftIndex;
+            }
+
+            if (rightIndex < this.length) {
+                if (
+                    (swapIndex === null &&
+                        this.comparator.compare(
+                            parent,
+                            this.values[rightIndex],
+                        )) > 0 ||
+                    (swapIndex !== null &&
+                        this.comparator.compare(
+                            this.values[leftIndex],
+                            this.values[rightIndex],
+                        )) > 0
+                ) {
+                    swapIndex = rightIndex;
+                }
+            }
+
+            if (swapIndex === null) break;
+            this.values[parentIndex] = this.values[swapIndex];
+            this.values[swapIndex] = parent;
+            parentIndex = swapIndex;
+        }
+    }
+}
+
+class Comparator {
+    constructor(compare = () => {}) {
+        if (compare) this.compare = compare;
+    }
+
+    compare(o1, o2) {
+        return o1 - o2;
+    }
 }
 
 const input = testCase => {
@@ -52,5 +150,6 @@ const testCase = `7
 10
 -99
 7
-5`;
+6
+2`;
 console.log(solution(input(testCase)));
