51d00b53079e71d93500611994039737ac714d6b
정진아 <human00024@naver.com>
2022-06-09 22:09:26 +0900

    [upload] _12914.멀리 뛰기 - 성공 코드

--- prog/level2/_12914_멀리뛰기.js
+++ prog/level2/_12914_멀리뛰기.js
@@ -3,22 +3,23 @@
  * 효진이는 한 번에 1칸 또는 2칸을 뛸 수 있다.
  * 이때 맨 끝 칸에 도달할 수 있는 방법이 몇 가지인지 알아내자.
  *
- * @param {number} n - 멀리 뛰에 사용될 칸의 수
+ * @param {number} n - 멀리 뛰에 사용될 칸의 수 (1~2000)
  * @returns - 끝에 도달하는 방법의 수
  */
 
 function solution(n) {
-    let count = 0;
-    jump(0);
-    return count % 1234567;
+    return countJump(n);
+}
 
-    function jump(point) {
-        if (point >= n) {
-            if (point === n) count++;
-            return;
-        }
-        jump(point + 1);
-        jump(point + 2);
+function countJump(n) {
+    const dp = Array(n + 1).fill(null);
+    dp[0] = dp[1] = 1;
+
+    return fibonacci(n);
+
+    function fibonacci(n) {
+        if (dp[n]) return dp[n];
+        return (dp[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567);
     }
 }
 
@@ -26,3 +27,4 @@ function solution(n) {
 
 console.log(solution(4));
 console.log(solution(3));
+console.log(solution(2000));
