b67e6c38a9e012f570ba5e3fa811ef6ba19afcd1
정진아 <human00024@naver.com>
2022-06-10 15:55:06 +0900

    [refactor] _12985.예상 대진표 - 함수 추출

--- prog/level2/_12985_예상대진표.js
+++ prog/level2/_12985_예상대진표.js
@@ -36,13 +36,17 @@
 function solution(n, a, b) {
     let round = 0;
     while (a !== b) {
-        a = Math.floor((a + 1) / 2);
-        b = Math.floor((b + 1) / 2);
+        a = calculateNextRoundNumber(a);
+        b = calculateNextRoundNumber(b);
 
         round++;
     }
 
     return round;
+}
+
+function calculateNextRoundNumber(number) {
+    return Math.floor((number + 1) / 2);
 }
 
 /****** TEST CASE *******/
