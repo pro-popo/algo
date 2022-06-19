faf9b9b80a2f3490ede18fcec76f0e39bd93a175
정진아 <human00024@naver.com>
2022-06-19 23:23:41 +0900

    [upload] _12900.2 x n 타일링 - 성공 코드

--- prog/level2/_12900_2xn타일링.js
+++ prog/level2/_12900_2xn타일링.js
@@ -10,8 +10,9 @@
  */
 
 function solution(n) {
-    const dp = Array(n + 1).fill(0);
-    [dp[0], dp[1]] = [1, 1];
+    const dp = Array(n + 1);
+    dp[0] = 1;
+    dp[1] = 1;
 
     for (let i = 2; i <= n; i++) {
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
