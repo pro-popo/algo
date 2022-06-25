7c7c276fb4ab95d3c5d05e7cffeb8a3fc53fdd63
정진아 <human00024@naver.com>
2022-06-25 13:16:58 +0900

    [upload] _2373.빙산

---
+++ boj/gold/Ⅳ/_2373_빙산.js
@@ -0,0 +1,100 @@
+/**
+ * 지구 온난화로 인해 빙산이 녹고 있다.
+ * 빙산의 각 부분별 높이 정보는 배열의 각 칸에 양의 정수로 저장된다.
+ * 바다의 경우 0으로 표현된다.
+ *
+ * 빙산의 높이는 바닷물에 많이 접해있는 부분에서 더 빨리 줄어든다.
+ * 빙산의 높이는 일년마다 그 칸에 동서남북 네 방향으로 붙어있는 0의 개수만큼 줄어든다.
+ *
+ * 한 덩어리의 빙산이 주어질 때,
+ * 이 빙산이 두 덩어리 이상으로 분이되는 최초의 시간을 구하자.
+ * 만약, 전부 다 녹을 때까지 두 덩어리 이상으로 분리되지 않는다면 0을 반환한다.
+ *
+ * @param {*} N - 행 (3~300)
+ * @param {*} M - 열 (3~300)
+ * @param {*} map - 값: 0~10, 빙산이 차지하는 칸의 개수: 10_000개 이하
+ */
+
+function solution(N, M, map) {
+    const dt = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+    ];
+
+    let time = 0;
+    do {
+        let isExistIceberg = false;
+        const visited = Array.from(Array(N), () => Array(M).fill(false));
+        for (let i = 0; i < N; i++) {
+            for (let j = 0; j < M; j++) {
+                if (map[i][j] <= 0 || visited[i][j]) continue;
+                if (isExistIceberg) return time;
+                isExistIceberg = true;
+
+                const queue = [[i, j]];
+                visited[i][j] = true;
+
+                while (queue.length) {
+                    const point = queue.shift();
+                    for (let d = 0; d < dt.length; d++) {
+                        const next = [point[0] + dt[d][0], point[1] + dt[d][1]];
+                        if (
+                            isOutOfRange(next) ||
+                            visited[next[0]][next[1]] ||
+                            map[next[0]][next[1]] <= 0
+                        )
+                            continue;
+
+                        queue.push(next);
+                        visited[next[0]][next[1]] = true;
+                    }
+                }
+            }
+        }
+        if (!isExistIceberg) break;
+
+        for (let i = 0; i < N; i++) {
+            for (let j = 0; j < M; j++) {
+                if (visited[i][j]) continue;
+
+                for (let d = 0; d < dt.length; d++) {
+                    const next = [i + dt[d][0], j + dt[d][1]];
+                    if (isOutOfRange(next)) continue;
+                    map[next[0]][next[1]]--;
+                }
+            }
+        }
+    } while (++time);
+
+    return 0;
+
+    function isOutOfRange(point) {
+        return point[0] < 0 || point[1] < 0 || point[0] === N || point[1] === M;
+    }
+}
+
+function input(test) {
+    const fs = require('fs');
+    const data = (
+        process.platform === 'linux'
+            ? fs.readFileSync('/dev/stdin').toString().trim()
+            : test
+    ).split('\n');
+
+    const [N, M] = data.shift().split(' ').map(Number);
+    const map = data.map(row => row.split(' ').map(Number));
+    return [N, M, map];
+}
+
+/****** TEST CASE *******/
+
+const TEST1 = `5 7
+0 0 0 0 0 0 0
+0 10 9 10 0 0 0
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0`;
+
+console.log(solution(...input(TEST1)));
